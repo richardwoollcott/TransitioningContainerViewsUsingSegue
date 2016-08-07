f7146749f1f9439b51f709ccc54c5b2bc26ad6ba
richardwoollcott <richardwoollcott@hotmail.com>
2016-08-07 07:14:25 +0100

    Initial Commit

---
+++ TransitioningContainerViewsUsingSegue/ContainerViewController.cs
@@ -0,0 +1,45 @@
+using Foundation;
+using System;
+using UIKit;
+using System.Threading.Tasks;
+
+namespace ContainerViews
+{
+    public partial class ContainerViewController : UIViewController
+    {
+        private NSString SegueIdentifierFirst = (NSString)"embedFirst";
+        private NSString SegueIdentifierSecond = (NSString)"embedSecond";
+
+        private TaskCompletionSource<bool> viewChangingTcs;
+
+        public ContainerViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public TaskCompletionSource<bool> ViewChangingTcs
+        {
+            get { return viewChangingTcs; }
+
+            set { viewChangingTcs = value; }
+        }
+
+        public Task<bool> PresentFirstViewAsync()
+        {
+            viewChangingTcs = new TaskCompletionSource<bool>();
+
+            PerformSegue(SegueIdentifierFirst, this);
+
+            return viewChangingTcs.Task;
+        }
+
+        public Task<bool> PresentSecondViewAsync()
+        {
+            viewChangingTcs = new TaskCompletionSource<bool>();
+
+            PerformSegue(SegueIdentifierSecond, this);
+
+            return viewChangingTcs.Task;
+        }
+
+    }
+}
